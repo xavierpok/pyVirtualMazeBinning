55a9a264694b894af5f8b2a8fca82683a803bdde
xavierpok <54708640+xavierpok@users.noreply.github.com>
2023-10-01 23:35:56 +0800

    Vectorise binner determination

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from main.IO import reading
 from main.binning import bin
+
 import csv
+from main.binning.match_bins import get_mappers
 
 if __name__ == "__main__":
     path = r"C:\Users\Xavier\Documents\GitHub\csvDiffs\unityfile_eyelinkFix.csv"
@@ -8,12 +10,15 @@ if __name__ == "__main__":
     print(f"Starting on :{path}, saving to {savepath}")
     numerical_vals = reading.read_numerical_vals(path)
     print(numerical_vals)
+    hitlocs = numerical_vals[:,1::]
     obj_names = reading.read_event_type(path)
     print(obj_names)
     with open(savepath, 'w', newline ='') as file :
         writer = csv.writer(file)
+        mappers = get_mappers(obj_names,hitlocs)
+        print("Got mappers successfully")
         for i in range(numerical_vals.shape[0]) :
-            #TODO : vectorise determination of binners
+            #TODO : vectorise determination of binners DONE
             #TODO : vectorise feeding into binners
             #TODO : vectorise conversion into offset
             
